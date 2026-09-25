f7ef25a7fe9730a159ae5a9180614e60a062e413
theangi <angishopps@gmail.com>
2026-09-24 23:26:12 -0600

    Reapply "adding revert console log"
    This reverts commit 328580e7c09a68b1f897ccba48fe23907c13401c.

--- hello.js
+++ hello.js
@@ -2,4 +2,3 @@ console.log("Hello git");
 console.log("Hello github");
 console.log("Trying out revert command");
 console.log("including a new console");
-console.log("including a bad console.log")
