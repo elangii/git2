9c996f3de712a18baca3540151b9bd8ee96c5969
theangi <angishopps@gmail.com>
2026-09-24 23:47:54 -0600

    trying out git stash

--- hello.js
+++ hello.js
@@ -2,3 +2,10 @@ console.log("Hello git");
 console.log("Hello github");
 console.log("Trying out revert command");
 console.log("including a new console");
+
+
+// i'm in the process of implementing an important feature
+// code
+// code
+// code
+// code
